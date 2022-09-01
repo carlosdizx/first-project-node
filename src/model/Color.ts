250274a667d8e7121101c87d795c149a68edd375
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-09-01 11:39:04 -0500

    Creado modelo que simulará los colores de un pixel

---
+++ src/model/Color.ts
@@ -0,0 +1,43 @@
+export class Color {
+    private _r: number;
+    private _g: number;
+    private _b: number;
+
+    constructor(r: number, g: number, b: number) {
+        if(!this.validarRango(r,g,b))
+            throw new Error("Valor fuera del rango, solo valores entre 0 y 255");
+        this._r = r;
+        this._g = g;
+        this._b = b;
+    }
+
+    validarRango = (r: number, g: number, b: number): boolean =>
+        r >= 0 && r <= 255 &&
+        g >= 0 && g <= 255 &&
+        b >= 0 && b <= 255;
+
+
+    get r(): number {
+        return this._r;
+    }
+
+    get g(): number {
+        return this._g;
+    }
+
+    get b(): number {
+        return this._b;
+    }
+
+    set r(value: number) {
+        this._r = value;
+    }
+
+    set g(value: number) {
+        this._g = value;
+    }
+
+    set b(value: number) {
+        this._b = value;
+    }
+}
