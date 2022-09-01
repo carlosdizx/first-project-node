4359802ecc1c26ed8b74789f32e0b1830f5748e1
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-09-01 11:57:36 -0500

    ajustes al package.json y creado el script del app.ts

---
+++ app.ts
@@ -0,0 +1,7 @@
+import { Imagen } from "./src/model/Imagen";
+
+const img1 = new Imagen(50, 50);
+const img2 = new Imagen(100, 100);
+const img3 = new Imagen(150, 100);
+const img4 = new Imagen(200, 200);
+console.log("xd");
